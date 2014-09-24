c9821271a75e82e80a9460d92ed0d7bfe60de935
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-09-24 12:12:32 +0200

    added the getMockGlobalsAdapter method

--- src/tad_TestableObject.php
+++ src/tad_TestableObject.php
@@ -4,30 +4,10 @@
 abstract class tad_TestableObject
 {
 
-    protected $F;
-    protected $G;
+    protected $f;
+    protected $g;
 
-    public function setFunctionsAdapter(tad_FunctionsAdapter $F = null)
-    {
-        $this->F = $F ? $F : new tad_FunctionsAdapter();
-    }
-
-    public function getFunctionsAdapter()
-    {
-        return $this->F;
-    }
-
-    public function setGlobalsAdapter(tad_GlobalsAdapterInterface $G = null)
-    {
-        $this->G = $G ? $G : new tad_GlobalsAdapter();
-    }
-
-    public function getGlobalsAdapter()
-    {
-        return $this->G;
-    }
-
-    public static function getMockFunctionsAdapter(PHPUnit_Framework_TestCase $testCase, $className = null)
+    protected static function getMockAdapter(PHPUnit_Framework_TestCase $testCase, $className, $notation, $toStubClassName, $alwaysStubMethods = null)
     {
         if (!is_string($className)) {
             // PHP >= 5.3
@@ -41,14 +21,23 @@ abstract class tad_TestableObject
         if (!class_exists($className)) {
             throw new InvalidArgumentException("Class $className does not exisit", 2);
         }
+        if (!is_string($notation)) {
+            throw new InvalidArgumentException('Comment notation must be a string', 3);
+        }
+        if (!is_string($toStubClassName)) {
+            throw new InvalidArgumentException('The name of the class to stub must be a string', 4);
+        }
+        if ($alwaysStubMethods && !is_array($alwaysStubMethods)) {
+            throw new InvalidArgumentException('Methods to always stub must come in an array', 5);
+        }
         $reflection = new ReflectionClass($className);
         $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
-        $toStub = array('__call');
+        $toStub = is_array($alwaysStubMethods) ? $alwaysStubMethods : array();
         foreach ($methods as $method) {
             if ($doc = $method->getDocComment()) {
                 $lines = explode("\n", $doc);
                 foreach ($lines as $line) {
-                    if (count($parts = explode('@F', $line)) > 1) {
+                    if (count($parts = explode('@' . $notation, $line)) > 1) {
                         $parts = trim(preg_replace("/[,;(; )(, )]+/", " ", $parts[1]));
                         $adapterMethodNames = explode(' ', $parts);
                         foreach ($adapterMethodNames as $adapterMethodName) {
@@ -60,6 +49,36 @@ abstract class tad_TestableObject
             }
         }
         $toStub = array_unique($toStub);
-        return $testCase->getMockBuilder('tad_FunctionsAdapterInterface')->setMethods($toStub)->getMock();
+        return $testCase->getMockBuilder($toStubClassName)->setMethods($toStub)->getMock();
     }
-} 
+
+    public function setFunctionsAdapter(tad_FunctionsAdapter $f = null)
+    {
+        $this->f = $f ? $f : new tad_FunctionsAdapter();
+    }
+
+    public function getFunctionsAdapter()
+    {
+        return $this->F;
+    }
+
+    public function setGlobalsAdapter(tad_GlobalsAdapterInterface $g = null)
+    {
+        $this->g = $g ? $g : new tad_GlobalsAdapter();
+    }
+
+    public function getGlobalsAdapter()
+    {
+        return $this->g;
+    }
+
+    public static function getMockFunctionsAdapter(PHPUnit_Framework_TestCase $testCase, $className = null)
+    {
+        return self::getMockAdapter($testCase, $className, 'f', 'tad_FunctionsAdapterInterface', array('__call'));
+    }
+
+    public static function getMockGlobalsAdapter(PHPUnit_Framework_TestCase $testCase, $className = null)
+    {
+        return self::getMockAdapter($testCase, $className, 'g', 'tad_GlobalsAdapterInterface', array('__call'));
+    }
+}
--- tests/unit/tad_TestableObjectTest.php
+++ tests/unit/tad_TestableObjectTest.php
@@ -1,70 +1,93 @@
 <?php
-class A extends  tad_TestableObject{
 
-	/**
-	 * @F functionOne functionTwo
-     * @G functionOne functionTwo
-	 */
-	public function methodOne(){
-
-	}
-}
-class B extends  tad_TestableObject{
+class A extends tad_TestableObject
+{
 
     /**
-     * @F functionOne functionTwo
-     * @G functionOne functionTwo
+     * @f functionOne functionTwo
+     * @g functionOne functionTwo
      */
-    public function methodOne(){
-
-    }
-    /**
-     * @F functionOne functionTwo
-     * @G functionOne functionTwo
-     */
-    public function methodTwo(){
+    public function methodOne()
+    {
 
     }
 }
-class C extends  tad_TestableObject{
 
-    public function methodOne(){
+class B extends tad_TestableObject
+{
+
+    /**
+     * @f functionOne functionTwo
+     * @g functionOne functionTwo
+     */
+    public function methodOne()
+    {
+
+    }
+
+    /**
+     * @f functionOne functionTwo
+     * @g functionOne functionTwo
+     */
+    public function methodTwo()
+    {
 
     }
 }
-class D extends  tad_TestableObject{
 
-    /**
-     * @F functionOne,functionTwo
-     * @G functionOne,functionTwo
-     */
-    public function methodOne(){
-    }
-    /**
-     * @F functionThree, functionFour
-     * @G functionThree, functionFour
-     */
-    public function methodTwo(){
-    }
-    /**
-     * @F functionFive;functionSix
-     * @G functionFive;functionSix
-     */
-    public function methodThree(){
-    }
-    /**
-     * @F functionSeven; functionEight
-     * @G functionSeven; functionEight
-     */
-    public function methodFour(){
-    }
-    /**
-     * @F functionNine functionTen
-     * @G functionNine functionTen
-     */
-    public function methodFive(){
+class C extends tad_TestableObject
+{
+
+    public function methodOne()
+    {
+
     }
 }
+
+class D extends tad_TestableObject
+{
+
+    /**
+     * @f functionOne,functionTwo
+     * @g functionOne,functionTwo
+     */
+    public function methodOne()
+    {
+    }
+
+    /**
+     * @f functionThree, functionFour
+     * @g functionThree, functionFour
+     */
+    public function methodTwo()
+    {
+    }
+
+    /**
+     * @f functionFive;functionSix
+     * @g functionFive;functionSix
+     */
+    public function methodThree()
+    {
+    }
+
+    /**
+     * @f functionSeven; functionEight
+     * @g functionSeven; functionEight
+     */
+    public function methodFour()
+    {
+    }
+
+    /**
+     * @f functionNine functionTen
+     * @g functionNine functionTen
+     */
+    public function methodFive()
+    {
+    }
+}
+
 class tad_TestableObjectTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
@@ -75,16 +98,17 @@ class tad_TestableObjectTest extends \PHPUnit_Framework_TestCase
     {
     }
 
-	/**
-	 * @test
-	 * it should return a mock functions adapter with mocked methods
-	 */
-	public function it_should_return_a_mock_functions_adapter_with_mocked_methods() {
-		$fMock = A::getMockFunctionsAdapter($this);
+    /**
+     * @test
+     * it should return a mock functions adapter with mocked methods
+     */
+    public function it_should_return_a_mock_functions_adapter_with_mocked_methods()
+    {
+        $fMock = A::getMockFunctionsAdapter($this);
         $this->assertTrue(method_exists($fMock, '__call'));
-		$this->assertTrue(method_exists($fMock, 'functionOne'));
-		$this->assertTrue(method_exists($fMock, 'functionTwo'));
-	}
+        $this->assertTrue(method_exists($fMock, 'functionOne'));
+        $this->assertTrue(method_exists($fMock, 'functionTwo'));
+    }
 
     /**
      * @test
@@ -92,10 +116,10 @@ class tad_TestableObjectTest extends \PHPUnit_Framework_TestCase
      */
     public function it_should_not_mock_the_same_functions_adapter_methods_twice()
     {
-		$fMock = B::getMockFunctionsAdapter($this);
+        $fMock = B::getMockFunctionsAdapter($this);
         $this->assertTrue(method_exists($fMock, '__call'));
-		$this->assertTrue(method_exists($fMock, 'functionOne'));
-		$this->assertTrue(method_exists($fMock, 'functionTwo'));
+        $this->assertTrue(method_exists($fMock, 'functionOne'));
+        $this->assertTrue(method_exists($fMock, 'functionTwo'));
     }
 
     /**
@@ -110,9 +134,9 @@ class tad_TestableObjectTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
-     * it should deal with different separators
+     * it should deal with different separators for the functions adapter methods
      */
-    public function it_should_deal_with_different_separators()
+    public function it_should_deal_with_different_separators_for_the_functions_adapter_methods()
     {
         $fMock = D::getMockFunctionsAdapter($this);
         $this->assertTrue(method_exists($fMock, '__call'));
@@ -126,5 +150,59 @@ class tad_TestableObjectTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(method_exists($fMock, 'functionEight'));
         $this->assertTrue(method_exists($fMock, 'functionNine'));
         $this->assertTrue(method_exists($fMock, 'functionTen'));
+    }
+
+    /**
+     * @test
+     * it should return a mock globals adapter with mocked methods
+     */
+    public function it_should_return_a_mock_globals_adapter_with_mocked_methods()
+    {
+        $gMock = A::getMockGlobalsAdapter($this);
+        $this->assertTrue(method_exists($gMock, '__call'));
+        $this->assertTrue(method_exists($gMock, 'functionOne'));
+        $this->assertTrue(method_exists($gMock, 'functionTwo'));
+    }
+
+    /**
+     * @test
+     * it should not mock the same globals adapters methods twice
+     */
+    public function it_should_not_mock_the_same_globals_adapter_methods_twice()
+    {
+        $gMock = B::getMockGlobalsAdapter($this);
+        $this->assertTrue(method_exists($gMock, '__call'));
+        $this->assertTrue(method_exists($gMock, 'functionOne'));
+        $this->assertTrue(method_exists($gMock, 'functionTwo'));
+    }
+
+    /**
+     * @test
+     * it should always mock the globals adapter call method
+     */
+    public function it_should_always_mock_the_globals_adpater_call_method()
+    {
+        $gMock = C::getMockGlobalsAdapter($this);
+        $this->assertTrue(method_exists($gMock, '__call'));
+    }
+
+    /**
+     * @test
+     * it should deal with different separators for the globals adapter methods
+     */
+    public function it_should_deal_with_different_separators_for_the_globals_adapter_methods()
+    {
+        $gMock = D::getMockGlobalsAdapter($this);
+        $this->assertTrue(method_exists($gMock, '__call'));
+        $this->assertTrue(method_exists($gMock, 'functionOne'));
+        $this->assertTrue(method_exists($gMock, 'functionTwo'));
+        $this->assertTrue(method_exists($gMock, 'functionThree'));
+        $this->assertTrue(method_exists($gMock, 'functionFour'));
+        $this->assertTrue(method_exists($gMock, 'functionFive'));
+        $this->assertTrue(method_exists($gMock, 'functionSix'));
+        $this->assertTrue(method_exists($gMock, 'functionSeven'));
+        $this->assertTrue(method_exists($gMock, 'functionEight'));
+        $this->assertTrue(method_exists($gMock, 'functionNine'));
+        $this->assertTrue(method_exists($gMock, 'functionTen'));
     }
 }
