26cd9e0cf489c7a1332325d48bb25a7fc8335351
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-09-26 07:41:58 +0200

    added check for the get_called_class function

--- src/tad_TestableObject.php
+++ src/tad_TestableObject.php
@@ -31,9 +31,12 @@ abstract class tad_TestableObject
         if (!is_string($methodName)) {
             throw new InvalidArgumentException('Method name must be a string', 1);
         }
+        if (!function_exists('get_called_class')) {
+            throw new RuntimeException('While the class is PHP 5.2 compatible the getMocksFor method is meant to be used in testing environment based on PHP >= 5.3 version.', 2);
+        }
         $className = get_called_class();
         if (!method_exists($className, $methodName)) {
-            throw new InvalidArgumentException("Method $methodName does not exist", 2);
+            throw new InvalidArgumentException("Method $methodName does not exist", 3);
         }
         $mocker = new tad_DependencyMocker($testCase, $className);
         return $mocker->setMethod($methodName)
