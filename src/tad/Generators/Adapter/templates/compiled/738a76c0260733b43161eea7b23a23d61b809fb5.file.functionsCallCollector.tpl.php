bab2cf27e560ded5be090260886e9bf7009c8b77
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-10-14 17:13:32 +0200

    updated the functions calls collector/adapter template

--- src/tad/Generators/Adapter/templates/compiled/738a76c0260733b43161eea7b23a23d61b809fb5.file.functionsCallCollector.tpl.php
+++ src/tad/Generators/Adapter/templates/compiled/738a76c0260733b43161eea7b23a23d61b809fb5.file.functionsCallCollector.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-10-09 18:29:19
+<?php /* Smarty version Smarty-3.1.19-dev, created on 2014-10-14 17:12:44
          compiled from "/Users/Luca/Dropbox/Developer/WebDeveloper/websites/php52/composer-packages/tdd-helpers/src/tad/Generators/Adapter/templates/functionsCallCollector.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:81576737054369a55637ab3-87135902%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '738a76c0260733b43161eea7b23a23d61b809fb5' => 
     array (
       0 => '/Users/Luca/Dropbox/Developer/WebDeveloper/websites/php52/composer-packages/tdd-helpers/src/tad/Generators/Adapter/templates/functionsCallCollector.tpl',
-      1 => 1412872100,
+      1 => 1413299534,
       2 => 'file',
     ),
   ),
@@ -42,57 +42,147 @@ namespace <?php echo $_smarty_tpl->tpl_vars['namespace']->value;?>
 class <?php echo $_smarty_tpl->tpl_vars['className']->value;?>
  <?php if ($_smarty_tpl->tpl_vars['interfaceName']->value) {?>implements <?php echo $_smarty_tpl->tpl_vars['interfaceName']->value;?>
 <?php }?> {
+    /**
+    * @var array
+    */
     protected $called;
+
+    /**
+    * @var string Absolute path to the the json file logged functions will be written to.
+    */
     protected $jsonFilePath;
+
+    /**
+    * @var bool
+    */
     protected $shouldAppend;
 
-    public function __construct()
-    {
-        $this->called = array();
-        $this->jsonFilePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'functions_dump' . time();
-        $this->shouldAppend = false;
-}
+    /**
+    * @var null|\PHPUnit_Framework_MockObject_MockObject
+    */
+    protected $mockObject;
 
+    /**
+    * @param array $called
+    * @param null $jsonFilePath
+    * @param bool $shoulAppend
+    * @param \PHPUnit_Framework_MockObject_MockObject $mockObject
+    */
+    public function __construct(array $called = null, $jsonFilePath = null, $shoulAppend = false, \PHPUnit_Framework_MockObject_MockObject $mockObject = null)
+{
+    $this->called = array();
+    $this->shouldAppend = false;
+    $this->mockObject = $mockObject ? $mockObject : null;
+    }
+
+    /**
+    * @param $function
+    * @param $arguments
+    * @return mixed
+    */
     public function __call($function, $arguments)
-    {
-        $reflectionFunction = new \ReflectionFunction($function);
-        $this->called[$reflectionFunction->name] = $reflectionFunction;
-        return call_user_func_array($function, $arguments);
+{
+    $reflectionFunction = new \ReflectionFunction($function);
+
+    $name = $reflectionFunction->name;
+    $args = array(
+    'name' => $name,
+    'parameters' => array()
+    );
+
+    foreach ($reflectionFunction->getParameters() as $param) {    $type = $param->getClass() ? $param->getClass()->name : false;
+    if (!$type && $param->isArray()) {    $type = 'array';
+    }
+    $args['parameters'][$param->name] = array(
+    'type' => $type,
+    'isPassedByReference' => $param->isPassedByReference(),
+    'name' => $param->name,
+    'isOptional' => $param->isOptional(),
+    'defaultValue' => $param->isDefaultValueAvailable() && $param->getDefaultValue() ? $param->getDefaultValue() : false
+    );
     }
 
+    $this->called[$name] = $args;
+
+    $responder = $this->mockObject ? array($this->mockObject, $function) : $function;
+
+    return call_user_func_array($responder, $arguments);
+    }
+
+    /**
+    * @return array The called functions information stored under their name.
+    */
     public function _getCalled()
-    {
-        return array_values($this->called);
+{
+    return $this->called;
     }
 
+    /**
+    * @param $jsonFilePath
+    * @return $this
+    * @throws \Exception
+    */
     public function _setJsonFilePath($jsonFilePath)
-    {
-        if (!is_string($jsonFilePath)) {
-        throw new \Exception('Json file path must be a string');
-        }
-        $this->jsonFilePath = $jsonFilePath;
+{
+    if (!is_string($jsonFilePath)) {    throw new \Exception('Json file path must be a string');
+    }
+    $this->jsonFilePath = $jsonFilePath;
+    return $this;
     }
 
+    /**
+    * @return string
+    */
     public function _getJsonFilePath()
-    {
-        return $this->jsonFilePath;
+{
+    return $this->jsonFilePath;
     }
 
     public function __destruct()
-    {
-        $jsonFilePath = $this->jsonFilePath ? $this->jsonFilePath : false;
-        if (!$jsonFilePath) {
-        return;
+{
+    $jsonFilePath = $this->jsonFilePath ? $this->jsonFilePath : false;
+    if (!$jsonFilePath) {    return;
     }
-        $onFile = $this->shouldAppend ? json_decode(@file_get_contents($this->jsonFilePath)) : array();
-        $contents = json_encode(array_merge($onFile, array_keys($this->called)));
-        @file_put_contents($this->jsonFilePath, $contents);
+    $onFile = array();
+    if ($this->shouldAppend) {    $onFile = json_decode(@file_get_contents($this->jsonFilePath), true);
+    }
+    $contents = json_encode(array_merge($onFile, $this->called));
+    @file_put_contents($this->jsonFilePath, $contents);
     }
 
+    /**
+    * Sets the log to be appended to the previously stored calls
+    * in place of overwriting them.
+    *
+    * @param bool $shouldAppend
+    * @return $this
+    */
     public function _shouldAppend($shouldAppend = true)
-    {
-        $this->shouldAppend = $shouldAppend ? true : false;
+{
+    $this->shouldAppend = $shouldAppend ? true : false;
+    return $this;
     }
 
+    /**
+    *  Returns the set mock object if any.
+    *
+    * @return null|\PHPUnit_Framework_MockObject_MockObject
+    */
+    public function _getMockObject()
+{
+    return $this->mockObject;
+    }
+
+    /**
+    * Sets the mock object that will respond to function calls.
+    *
+    * @param \PHPUnit_Framework_MockObject_MockObject $mockObject
+    * @return $this
+    */
+    public function _setMockObject(\PHPUnit_Framework_MockObject_MockObject $mockObject = null)
+{
+    $this->mockObject = $mockObject;
+    return $this;
+    }
 }
 <?php }} ?>
--- src/tad/Generators/Adapter/templates/functionsCallCollector.tpl
+++ src/tad/Generators/Adapter/templates/functionsCallCollector.tpl
@@ -8,56 +8,146 @@ namespace {$namespace};
 {if $classComment}{$classComment}
 {/if}
 class {$className} {if $interfaceName}implements {$interfaceName}{/if} {literal}{{/literal}
+    /**
+    * @var array
+    */
     protected $called;
+
+    /**
+    * @var string Absolute path to the the json file logged functions will be written to.
+    */
     protected $jsonFilePath;
+
+    /**
+    * @var bool
+    */
     protected $shouldAppend;
 
-    public function __construct()
-    {literal}{{/literal}
-        $this->called = array();
-        $this->jsonFilePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'functions_dump' . time();
-        $this->shouldAppend = false;
-{literal}}{/literal}
+    /**
+    * @var null|\PHPUnit_Framework_MockObject_MockObject
+    */
+    protected $mockObject;
 
+    /**
+    * @param array $called
+    * @param null $jsonFilePath
+    * @param bool $shoulAppend
+    * @param \PHPUnit_Framework_MockObject_MockObject $mockObject
+    */
+    public function __construct(array $called = null, $jsonFilePath = null, $shoulAppend = false, \PHPUnit_Framework_MockObject_MockObject $mockObject = null)
+{literal}{{/literal}
+    $this->called = array();
+    $this->shouldAppend = false;
+    $this->mockObject = $mockObject ? $mockObject : null;
+    {literal}}{/literal}
+
+    /**
+    * @param $function
+    * @param $arguments
+    * @return mixed
+    */
     public function __call($function, $arguments)
-    {literal}{{/literal}
-        $reflectionFunction = new \ReflectionFunction($function);
-        $this->called[$reflectionFunction->name] = $reflectionFunction;
-        return call_user_func_array($function, $arguments);
+{literal}{{/literal}
+    $reflectionFunction = new \ReflectionFunction($function);
+
+    $name = $reflectionFunction->name;
+    $args = array(
+    'name' => $name,
+    'parameters' => array()
+    );
+
+    foreach ($reflectionFunction->getParameters() as $param) {literal}{{/literal}    $type = $param->getClass() ? $param->getClass()->name : false;
+    if (!$type && $param->isArray()) {literal}{{/literal}    $type = 'array';
+    {literal}}{/literal}
+    $args['parameters'][$param->name] = array(
+    'type' => $type,
+    'isPassedByReference' => $param->isPassedByReference(),
+    'name' => $param->name,
+    'isOptional' => $param->isOptional(),
+    'defaultValue' => $param->isDefaultValueAvailable() && $param->getDefaultValue() ? $param->getDefaultValue() : false
+    );
     {literal}}{/literal}
 
+    $this->called[$name] = $args;
+
+    $responder = $this->mockObject ? array($this->mockObject, $function) : $function;
+
+    return call_user_func_array($responder, $arguments);
+    {literal}}{/literal}
+
+    /**
+    * @return array The called functions information stored under their name.
+    */
     public function _getCalled()
-    {literal}{{/literal}
-        return array_values($this->called);
+{literal}{{/literal}
+    return $this->called;
     {literal}}{/literal}
 
+    /**
+    * @param $jsonFilePath
+    * @return $this
+    * @throws \Exception
+    */
     public function _setJsonFilePath($jsonFilePath)
-    {literal}{{/literal}
-        if (!is_string($jsonFilePath)) {literal}{{/literal}
-        throw new \Exception('Json file path must be a string');
-        {literal}}{/literal}
-        $this->jsonFilePath = $jsonFilePath;
+{literal}{{/literal}
+    if (!is_string($jsonFilePath)) {literal}{{/literal}    throw new \Exception('Json file path must be a string');
+    {literal}}{/literal}
+    $this->jsonFilePath = $jsonFilePath;
+    return $this;
     {literal}}{/literal}
 
+    /**
+    * @return string
+    */
     public function _getJsonFilePath()
-    {literal}{{/literal}
-        return $this->jsonFilePath;
+{literal}{{/literal}
+    return $this->jsonFilePath;
     {literal}}{/literal}
 
     public function __destruct()
-    {literal}{{/literal}
-        $jsonFilePath = $this->jsonFilePath ? $this->jsonFilePath : false;
-        if (!$jsonFilePath) {literal}{{/literal}
-        return;
+{literal}{{/literal}
+    $jsonFilePath = $this->jsonFilePath ? $this->jsonFilePath : false;
+    if (!$jsonFilePath) {literal}{{/literal}    return;
     {literal}}{/literal}
-        $onFile = $this->shouldAppend ? json_decode(@file_get_contents($this->jsonFilePath)) : array();
-        $contents = json_encode(array_merge($onFile, array_keys($this->called)));
-        @file_put_contents($this->jsonFilePath, $contents);
+    $onFile = array();
+    if ($this->shouldAppend) {literal}{{/literal}    $onFile = json_decode(@file_get_contents($this->jsonFilePath), true);
+    {literal}}{/literal}
+    $contents = json_encode(array_merge($onFile, $this->called));
+    @file_put_contents($this->jsonFilePath, $contents);
     {literal}}{/literal}
 
+    /**
+    * Sets the log to be appended to the previously stored calls
+    * in place of overwriting them.
+    *
+    * @param bool $shouldAppend
+    * @return $this
+    */
     public function _shouldAppend($shouldAppend = true)
-    {literal}{{/literal}
-        $this->shouldAppend = $shouldAppend ? true : false;
+{literal}{{/literal}
+    $this->shouldAppend = $shouldAppend ? true : false;
+    return $this;
     {literal}}{/literal}
 
+    /**
+    *  Returns the set mock object if any.
+    *
+    * @return null|\PHPUnit_Framework_MockObject_MockObject
+    */
+    public function _getMockObject()
+{literal}{{/literal}
+    return $this->mockObject;
+    {literal}}{/literal}
+
+    /**
+    * Sets the mock object that will respond to function calls.
+    *
+    * @param \PHPUnit_Framework_MockObject_MockObject $mockObject
+    * @return $this
+    */
+    public function _setMockObject(\PHPUnit_Framework_MockObject_MockObject $mockObject = null)
+{literal}{{/literal}
+    $this->mockObject = $mockObject;
+    return $this;
+    {literal}}{/literal}
 {literal}}{/literal}
